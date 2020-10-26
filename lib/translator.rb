247f6d7a8bc4ab3548f380b0479f43120817b8c1
Avelon Pang <avelonpang@gmail.com>
2020-10-26 23:11:50 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -30,8 +30,8 @@ result
 end 
 
 
-def get_japanese_emoticon(emoticons_file, emoticon)
+#def get_japanese_emoticon(emoticons_file, emoticon)
   #takes eng emoticon and translate to jap emoticon 
   
-end
+#end
 
