35e655702ab25f0a3138ff54444daf3b58e71869
Avelon Pang <avelonpang@gmail.com>
2020-10-26 22:51:03 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,13 +1,26 @@
 # require modules here
+# Write a method that loads the emoticons.yml file.
+require 'yaml'
+require 'pry'
 
-def load_library
-  # code goes here
+def load_library(emoticons)
+  emoticons = YAML.load_file('./lib/emoticons.yml')
+  outer_hash = {}
+  emoticons.each do |meaning, value|  
+outer_hash[meaning] = {}
+outer_hash[meaning][:english] = value[0]
+outer_hash[meaning][:japanese] = value[1]
+  end
+  outer_hash
 end
 
-def get_japanese_emoticon
-  # code goes here
+
+
+def get_japanese_emoticon(emoticons, emoticon)
+  
 end
 
-def get_english_meaning
-  # code goes here
-end
+
+def get_english_meaning(emoticons, emoticon)
+ 
+end 
